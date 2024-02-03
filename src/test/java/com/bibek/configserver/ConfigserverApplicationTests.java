b0439fb56f569f1cfbc379f7701114ca5818b539
Debta <bibekananda.debta@capgemini.com>
2024-02-03 12:12:08 +0530

    test

---
+++ src/test/java/com/bibek/configserver/ConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bibek.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
